d5be3e56116ba1ede1de33d78460ef66d53a12cb
Tommy Blomskog <tommy.blomskog@sokigo.com>
2021-11-15 09:05:59 +0100

    Add sln

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Client
+{
+    /// <summary>
+    /// This is the starter-kit that we use in the training course IdentityServer in Production  
+    /// by https://www.tn-data.se
+    /// </summary>
+    public class Program
+    {
+        private static readonly string _applicationName = "Client";
+
+        public static void Main(string[] args)
+        {
+            Settings.StartupTime = DateTime.Now;
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ PaymentAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentAPI.Controllers
+{
+    /// <summary>
+    /// This is the starter-kit that we use in the training course IdentityServer in Production  
+    /// by https://www.tn-data.se
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
