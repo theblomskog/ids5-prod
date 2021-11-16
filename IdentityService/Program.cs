bf7dbd6db4128c99092c7e804b7406037555251e
Tommy Blomskog <tommy.blomskog@sokigo.com>
2021-11-16 10:17:03 +0100

    Add serilog

--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace IdentityService
 {
@@ -18,12 +20,36 @@ namespace IdentityService
     {
         private static readonly string _applicationName = "IdentityService";
 
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             Settings.StartupTime = DateTime.Now;
             Console.Title = _applicationName;
 
-            CreateHostBuilder(args).Build().Run();
+
+            try
+            {
+                Log.Information("Starting web host for " + _applicationName);
+                CreateHostBuilder(args)
+                    .ConfigureSerilog(_applicationName, options =>
+                    {
+                        ConfigureLogLevels(options);
+                    })
+                    .UseSerilog()
+                    .Build()
+                    .Run();
+                return 0;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -73,6 +99,43 @@ namespace IdentityService
                         {
                             webBuilder.UseStartup<Startup>();
                         });
+            }
+        }
+
+        private static void ConfigureLogLevels(LoggerConfiguration options)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            switch (environmentName)
+            {
+                case "Production":
+                    options.MinimumLevel.Override("System", LogEventLevel.Warning)
+                           .MinimumLevel.Override("IdentityServer4", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.DataProtection", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authorization", LogEventLevel.Information);
+                    break;
+                case "Offline":
+                    options.MinimumLevel.Override("System", LogEventLevel.Information)
+                           .MinimumLevel.Override("IdentityServer4", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                           .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.DataProtection", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authorization", LogEventLevel.Information);
+                    break;
+                default:
+                    //Development
+                    options.MinimumLevel.Override("System", LogEventLevel.Warning)
+                           .MinimumLevel.Override("IdentityServer4", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                           .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.DataProtection", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                           .MinimumLevel.Override("Microsoft.AspNetCore.Authorization", LogEventLevel.Information);
+                    break;
             }
         }
     }
